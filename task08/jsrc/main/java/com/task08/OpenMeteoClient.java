17da9106b0bd593f2e498aaaac04e10b0ddb9a14
Iryna Raskosova <Iryna_Raskosova@epam.com>
2024-05-25 20:15:42 +0200

    Update task08

--- task08/jsrc/main/java/com/task08/OpenMeteoClient.java
+++ task08/jsrc/main/java/com/task08/OpenMeteoClient.java
@@ -8,13 +8,14 @@ import org.apache.http.util.EntityUtils;
 
 public class OpenMeteoClient {
     private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast";
+    private static final String API_URL = "https://api.open-meteo.com/v1/forecast?latitude=50.4375&longitude=30.5&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&current_weather=true";
     private static final String PARAMS = "?latitude={lat}&longitude={lon}&current_weather=true";
 
     public String getWeather(double latitude, double longitude) throws Exception {
         String url = BASE_URL + PARAMS.replace("{lat}", String.valueOf(latitude))
                 .replace("{lon}", String.valueOf(longitude));
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(url);
+            HttpGet request = new HttpGet(API_URL);
             try (CloseableHttpResponse response = httpClient.execute(request)) {
                 return EntityUtils.toString(response.getEntity());
             }
